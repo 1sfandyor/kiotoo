8f2909b109403f5b55a57164353eb3eaaea216fc
1sfandyor <isfandiyor06112000@gmail.com>
2024-10-24 19:37:30 +0500

    dsadasd

--- src/hooks/use-outside-click.ts
+++ src/hooks/use-outside-click.ts
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 
 export const useOutsideClick = (
   ref: React.RefObject<HTMLDivElement>,
-  callback: Function
+  callback: (event: MouseEvent | TouchEvent) => void // Explicitly define the callback type
 ) => {
   useEffect(() => {
-    const listener = (event: any) => {
-      if (!ref.current || ref.current.contains(event.target)) {
+    const listener = (event: MouseEvent | TouchEvent) => { // Changed from 'any' to 'MouseEvent | TouchEvent'
+      if (!ref.current || ref.current.contains(event.target as Node)) { // Added type assertion
         return;
       }
       callback(event);

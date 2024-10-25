9b62d44493da949f99a99ff7b1e46366d21aadcd
1sfandyor <isfandiyor06112000@gmail.com>
2024-10-25 15:49:41 +0500

    finished

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -172,10 +172,10 @@ const News = () => {
         {/* GlareCard ni postlar ro'yxatidan tashqarida joylashtirdik */}
         {
           !mobile && (
-            <div className={`fixed z-50 right-0 -translate-x-1/2 translate-y-1/3 lg:translate-y-1/4 2xl:translate-y-1/4 perspective-800 ${show ? 'block transition-all duration-400 ease-in' : 'hidden'}`}>
-              <div className='lg:w-[330px] lg:h-[450px] xl:w-[340px] xl:h-[500px] 2xl:w-[396px] 2xl:h-[584px] border pt-2.5 transform -rotate-y-[20deg] pr-3'>
+            <div className={`fixed z-50 right-0 -translate-x-1/2 translate-y-1/3 lg:translate-y-1/4 xl:-translate-x-1/4 xl:translate-y-2/4 2xl:translate-y-1/4 perspective-800 ${show ? 'block transition-all duration-400 ease-in' : 'hidden'}`}>
+              <div className='lg:w-[330px] lg:h-[450px] xl:w-[340px] xl:h-[500px] 2xl:w-[306px] 2xl:h-[204px] border pt-2.5 transform -rotate-y-[20deg] pr-3'>
                 <GlareCard className="flex flex-col items-center justify-center p-5 w-full h-full">
-                  <p className="text-gray-2/40 font-bold text-sm  lg:text-2xl xl:text-3xl mt-4 uppercase">{postData?.type}</p>
+                  <p className="text-gray-2/40 font-bold text-sm lg:text-2xl xl:text-3xl mt-4 uppercase">{postData?.type}</p>
                 </GlareCard>
               </div>
             </div>
